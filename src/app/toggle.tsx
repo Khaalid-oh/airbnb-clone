d42d5d264d0f90324a6bcd57a77c71da5c28f9f2
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-07-14 21:47:43 +0100

    fix: fix navbar responsiveness

--- src/app/toggle.tsx
+++ src/app/toggle.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { BiCheck } from "react-icons/bi";
@@ -19,7 +19,7 @@ function Toggle() {
         checked={isToggled}
         onChange={handleToggle}
       />
-      <div className="w-14 h-8 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-300 dark:peer-focus:ring-gray-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all  dark:border-gray-600 peer-checked:bg-gray-900 checked:z-20">
+      <div className="w-14 h-8 bg-gray-200 peer-focus:outline-none  peer-focus:ring-gray-300 rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all  dark:border-gray-600 peer-checked:bg-gray-900 checked:z-20">
         {isToggled && (
           <BiCheck className="text-xl z-10 absolute right-[6px] top-[6px]" />
         )}
